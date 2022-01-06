273270f84e7b5fbd55a46ddaa6eadbcc3c706d65
Maria Garcia <65758553+mupikga@users.noreply.github.com>
2022-01-06 17:18:45 +0100

    Commit3

--- src/StackImpl.java
+++ src/StackImpl.java
@@ -1,7 +1,7 @@
 public class StackImpl<E> implements Stack<E>{
 
-    E[] data;
-    private int p;
+    private E[] data;
+    private int i=0;
 
     public StackImpl(int len){
         data = (E[])new Object[len];
@@ -9,20 +9,28 @@ public class StackImpl<E> implements Stack<E>{
 
     @Override
     public void push(E e) throws FullStackException {
-        this.data[this.p++] = e;
-
-        if (true) throw new FullStackException();
+        if (i < data.length){
+            data[i]=e;
+            i++;
+        }
+        else
+            throw new FullStackException();
     }
 
     @Override
     public E pop() throws EmptyStackException {
-
-        if (this.p==0) throw new EmptyStackException();
-        return null;
+        if (data.length==0) throw new EmptyStackException();
+        int u = 0;
+        while (u<data.length) {
+            System.out.println("Pop: "+ data[u]);
+            u++;
+        }
+        E e= data[u-1];
+        return e;
     }
 
     @Override
     public int size() {
-        return 0;
+        return data.length;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -2,17 +2,14 @@ public class Test {
     public static void main(String[] args){
         System.out.println("TEST");
 
-        Stack<Integer> s = new StackImpl<Integer>(5);
+        Stack<Integer> s = new StackImpl<Integer>(3);
 
         try {
-            System.out.println(s.size());
-            s.push(7);
-            System.out.println(s.size());
-            s.push(12);
-            System.out.println(s.size());
-            s.push(20);
-            System.out.println(s.size());
+            System.out.println("Size: " + s.size());
 
+            s.push(7);
+            s.push(14);
+            s.push(28);
             s.pop();
 
         } catch (EmptyStackException empty){
